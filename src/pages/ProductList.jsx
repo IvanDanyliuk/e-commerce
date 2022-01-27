e14ea3f6fd0b4399ad01b5434565256f09645fbe
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-01-27 22:08:50 +0200

    Create the Shopping Categories page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+import Announcements from "../components/Announcements";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+
+const Container = styled.div`
+
+`;
+
+const Title = styled.h2`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  margin-right: 20px;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Select = styled.select`
+  margin-right: 20px;
+  padding: 10px;
+`;
+
+const Option = styled.option`
+
+`;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcements />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>Color</Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
