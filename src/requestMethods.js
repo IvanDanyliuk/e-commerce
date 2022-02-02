3114b51024b16810feac88ee0f769314a446d895
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-02-02 17:00:31 +0200

    Getting access token refactoring

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjgwNzkwNzhjNzU5OWY0NDg0ZjNhZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzY1MDY5OSwiZXhwIjoxNjQzOTA5ODk5fQ.lJ95apozg64wnJrm6niw-4gmn59VH8-rjyTPcqiElDo';
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
